31f98ec7674009c1b302245d8bb1a1166a0e1a6b
lorexp <vinixp.vp@gmail.com>
2019-12-04 16:08:42 -0300

    utilizando try catch

--- src/index.js
+++ src/index.js
@@ -6,38 +6,58 @@ const app = express();
 app.use(express.json());
 
 app.get("/bills", async (req, res) => {
-  const bills = await bill.findAll();
-  res.send(bills);
+  try {
+    const bills = await bill.findAll();
+    res.send(bills);
+  } catch (e) {
+    res.send(e);
+  }
 });
 
 app.get("/bill/:id", async (req, res) => {
-  const Bill = await bill.findByPk(req.params.id);
-  res.send(Bill);
+  try {
+    const Bill = await bill.findByPk(req.params.id);
+    res.send(Bill);
+  } catch (e) {
+    res.send(e);
+  }
 });
 
 app.post("/bill", async (req, res) => {
-  const Bill = await bill.create(req.body);
-  res.json(Bill);
+  try {
+    const Bill = await bill.create(req.body);
+    res.json(Bill);
+  } catch (e) {
+    res.send(e);
+  }
 });
 
 app.delete("/bill/:id", async (req, res) => {
-  const id = parseInt(req.params.id);
-  const deleted = await bill.destroy({
-    where: {
-      id
-    }
-  });
-  res.json(deleted);
+  try {
+    const id = parseInt(req.params.id);
+    const deleted = await bill.destroy({
+      where: {
+        id
+      }
+    });
+    res.json(deleted);
+  } catch (e) {
+    res.send(e);
+  }
 });
 
 app.put("/bill/:id", (req, res) => {
-  const id = req.params.id;
-  const updated = bill.update(req.body, {
-    where: {
-      id
-    }
-  });
-  res.json(updated);
+  try {
+    const id = req.params.id;
+    const updated = bill.update(req.body, {
+      where: {
+        id
+      }
+    });
+    res.json(updated);
+  } catch (e) {
+    res.send(e);
+  }
 });
 
 app.listen(3000, () => console.log("Listening..."));
